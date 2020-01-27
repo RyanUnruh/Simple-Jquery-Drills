7befd88d24c5297a270f292b7117c972bb0dba55
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-01-27 16:17:20 -0600

    4pm

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 $(document).ready(function () {
 
-
-
-
-
-  $('body').append("<div id='div-id'></div>");
-  
-
-
+  // Button
   let btnSubmit = $("#btnSubmit")
 
+  // Append li to ul
+
+  $('body').append("<ul id='ul-id'></ul>");
   btnSubmit.click((e) => {
-   e.preventDefault();
-   $('#div-id').append($("<h1></h1>").text($('#form-info').val()));
+    e.preventDefault();
+
+    let element = $('<li class="li"></li>')
+    $('#ul-id').append(element.text($('#form-info').val()))
+
+    // Change BG color of list item
+    element.click(function () {
+      element.css({
+        "background-color": getRandomColor(),
+        "border-radius": "5px",
+        "width": "10%"
+      })
+    });
+
+    element.dblclick(function () {
+      element.remove();
+    });
+
+
+    function getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+
+
+    function setRandomColor() {
+      $("#colorpad").css("background-color", getRandomColor());
+    }
+
   });
 
-
+  // Disable
   $('#form-info').keyup(function () {
     if ($(this).val() === '') {
       btnSubmit.attr('disabled', true);
@@ -23,8 +51,7 @@ $(document).ready(function () {
       btnSubmit.attr('disabled', false);
     }
   });
-  // e.preventDefault();
-  // $('#div-id').append($('<h1></h1>').text($('#first').val()));
+
 });
 
 
